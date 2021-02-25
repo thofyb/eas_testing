6fd6995e940f19d93844f7fcca2c2bfb03a86a8d
bozhnyukAlex <bozhnyuks@mail.ru>
2021-02-25 19:48:34 +0200

    some changes in test_typing

--- mrScripts/test_typing.py
+++ mrScripts/test_typing.py
@@ -4,7 +4,7 @@ import time
 
 device = MonkeyRunner.waitForConnection()
 
-device.installPackage("apks/Notes_v2.3.3_apkpure.com.apk")
+device.installPackage("apk/Notes_v2.3.3_apkpure.com.apk")
 
 package = "com.ogden.memo"
 
@@ -15,9 +15,10 @@ runComponent = package + "/" + activity
 device.startActivity(component=runComponent)
 MonkeyRunner.sleep(3)
 
-print("new note...")
+print("adding new note")
 device.touch(1000, 170, "DOWN_AND_UP")
 
+print("start_typing")
 start_sec = time.time()
 current_sec = time.time()
 while current_sec - start_sec < 1500:
